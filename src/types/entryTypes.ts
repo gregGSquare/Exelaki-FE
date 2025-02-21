1809ec0328a7a9ddfed9812b18fae04ec7fe2be4
gregkok <grkokkoris@gmail.com>
2025-02-22 00:02:49 +0100

    added type to entry model

--- src/types/entryTypes.ts
+++ src/types/entryTypes.ts
@@ -3,6 +3,7 @@ import { Category } from "./categoryTypes";
 export type EntryFlexibility = 'FIXED' | 'FLEXIBLE' | 'OPTIONAL';
 export type EntryRecurrence = 'MONTHLY' | 'QUARTERLY' | 'YEARLY' | 'ONE_TIME';
 export type EntryTags = 'HOUSING' | 'UTILITIES' | 'TRANSPORTATION' | 'FOOD' | 'DEBT' | 'INSURANCE' | 'SUBSCRIPTION' | 'ENTERTAINMENT' | 'MEDICAL' | 'MISC';
+export type EntryType = 'EXPENSE' | 'INCOME' | 'SAVING';
 
 export interface Entry {
   _id: string;
@@ -15,12 +16,14 @@ export interface Entry {
   recurrence: EntryRecurrence;
   tags: EntryTags[];
   category: Category;
+  type: EntryType;
 }
 
 export interface CreateEntryPayload {
   name: string;
   amount: number;
   categoryId: string;
+  type: EntryType;
   budgetId: string;
   dueDayOfMonth?: number;
   flexibility: EntryFlexibility;
